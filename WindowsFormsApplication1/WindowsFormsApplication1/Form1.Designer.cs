6de3819868fee25c5a242bdb6ecf47bf7a823310
Rafael Oliveira <rafaahuk@gmail.com>
2013-09-03 09:08:45 -0300

    imc

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.btn_Sair = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_peso.Location = new System.Drawing.Point(139, 23);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(119, 29);
+            this.txt_peso.TabIndex = 0;
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_altura.Location = new System.Drawing.Point(139, 96);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(119, 29);
+            this.txt_altura.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Peso :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Altura :";
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calc.Location = new System.Drawing.Point(282, 23);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(119, 103);
+            this.btn_calc.TabIndex = 4;
+            this.btn_calc.Text = "Calcular IMC";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sair.Location = new System.Drawing.Point(282, 159);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(119, 38);
+            this.btn_Sair.TabIndex = 5;
+            this.btn_Sair.Text = "Sair";
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpar.Location = new System.Drawing.Point(139, 159);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(119, 38);
+            this.btn_limpar.TabIndex = 6;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 237);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.btn_Sair);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.txt_peso);
+            this.Name = "Form1";
+            this.Text = "Calculo de IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.Button btn_Sair;
+        private System.Windows.Forms.Button btn_limpar;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float IMC, PESO, ALTURA;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            PESO = float.Parse(txt_peso.Text);
+            ALTURA = float.Parse(txt_altura.Text);
+
+            IMC = PESO / (ALTURA * ALTURA);
+
+            if (IMC <= 18.5)
+                MessageBox.Show(" IMC :  " +IMC+  "               BAIXO PESO  :  Nesse ponto , o corpo deixa de ser saudável e o organismo fica mais vulnerável a infecções. Se o baixo peso persistir , mesmo com alimentação normal , procure ajuda medica ; a dificuldade para engordar pode ser sintoma de alguma doença insidiosa , como o diabetes.");
+
+            if (IMC > 18.5 && IMC <= 24.9)
+                MessageBox.Show(" IMC :  " +IMC+ "               PESO IDEAL  :  Seu peso esta adequado à altura. É importante manter a educação alimentar e a atividade física. Fique atento caso seu valor esteja perto dos limites para os estágios de Baixo Peso ou Sobrepeso.");
+
+            if (IMC > 24.9 && IMC <= 29.9)
+                MessageBox.Show(" IMC :  " +IMC+ "               SOBREPESO   : Esta faixa indica que você esta com predisposição a obesidade; dependendo do seu historico familiar e pessoal , pode haver um quadro pré-diabetes e hipertensão. Procure orientação médica para evitar o proximo estágio , Obesidade 1 . Quanto mais quilos extras , maior a dificuldade para emagrecer.");
+
+            if (IMC > 29.9 && IMC <= 34.9)
+                MessageBox.Show(" IMC :  " +IMC+ "               OBESIDADE GRAU 1   :  O risco de desenvolver diabetes, para quem está nesta faixa de peso , é de 20% e o hipertensão ultrapassa 25%. Há risco maior de outras doençãs cardiovasculares , articulares , distúrbios psiquiatricos , apneia de sono e até certos tipos de câncer.");
+
+            if (IMC > 34.9 && IMC <= 39.9)
+                MessageBox.Show(" IMC :  " + IMC + "               OBESIDADE GRAU 2   : O risco de desenvolver diabetes chega a 40%, e a chance de surgirem doenças associadas à obesidade , como reumatismos , câncer , apneia do sono , hipertensão e outros problemas cardiovasculares pode chegar a 50%. Procure orientaçao médica.");
+
+            if (IMC >= 40)
+                MessageBox.Show(" IMC :  " + IMC + "               OBESIDADE GRAU 3   : O risco de desenvolver doenças associadas ao excesso de peso , como diabetes, reumatismos , câncer , apneia do sono , hipertensão e outros problemas cardiovasculares chega a até 90% . Nesse estágio , a cirurgia de reduçãodo estomago pode ser indicada. Procure orientação médica imediatamente.");
+
+
+            txt_altura.Clear();
+            txt_peso.Clear();
+
+            txt_peso.Focus();
+
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+
+            txt_altura.Clear();
+            txt_peso.Clear();
+
+            txt_peso.Focus();
+        }
+    }
+}
